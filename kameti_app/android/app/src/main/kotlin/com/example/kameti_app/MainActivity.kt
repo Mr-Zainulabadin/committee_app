2b2cf126ae87efdd2ea5d7fdfaf06e8f9e1fea53
ZAIN UL ABADIN <zaincomsatsvhr@gmail.com>
2024-06-27 05:56:05 +0500

    2

--- kameti_app/android/app/src/main/kotlin/com/example/kameti_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kameti_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- kameti_app/lib/utils/InternatConnectionStatus/provider/DependencyInjection.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:get/get.dart';
-// import 'package:kameti_app/utils/InternatConnectionStatus/provider/InternatConnectionProvider.dart';
-
-// class DependencyInjection {
-//   static void init() {
-//     Get.put(NetworkProvider(), permanent: true);
-//   }
-// }
